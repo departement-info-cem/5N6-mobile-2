e29c423e86e13219560129c23e2f33895ac432de
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2026-07-15 11:47:03 -0400

    WIP tllp

--- code/tape_le_lapin/lib/lapin.dart
+++ code/tape_le_lapin/lib/lapin.dart
@@ -10,6 +10,58 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Center(child: Text('Tape le lapin')));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text('Tape le 🐇', style: TextStyle(fontSize: 40)),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "Bonk : x",
+                  style: TextStyle(color: Colors.green, fontSize: 30),
+                ),
+                Text(
+                  "Zloop : y",
+                  style: TextStyle(color: Colors.red, fontSize: 30),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: null,
+                      child: Text("🐹", style: TextStyle(fontSize: 70)),
+                    ),
+                    ElevatedButton(
+                      onPressed: null,
+                      child: Text("🐹", style: TextStyle(fontSize: 70)),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: null,
+                      child: Text("🐹", style: TextStyle(fontSize: 70)),
+                    ),
+                    ElevatedButton(
+                      onPressed: null,
+                      child: Text("🐹", style: TextStyle(fontSize: 70)),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
