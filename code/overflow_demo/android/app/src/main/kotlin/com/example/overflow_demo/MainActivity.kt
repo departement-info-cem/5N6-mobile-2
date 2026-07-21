5e6fc45f6f2175c4a8ee9bd079b6dd13b10d96ac
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2026-07-21 11:16:19 -0400

    overflow wip

---
+++ code/overflow_demo/android/app/src/main/kotlin/com/example/overflow_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.overflow_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ code/overflow_demo/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Overflow Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Overflow Demo 🚧'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(width: 10000, height: 1000, color: Colors.blue),
+      ),
+    );
+  }
+}
